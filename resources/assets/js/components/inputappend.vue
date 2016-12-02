124bb2908106a305d0059168b21b2600aa08719b
TheRavingViking <sjoerdvangalen93@gmail.com>
2016-12-02 21:52:11 +0100

    add recipe and average from ratings

---
+++ resources/assets/js/components/inputappend.vue
@@ -0,0 +1,35 @@
+<div class="input_fields_wrap">
+    <button class="add_field_button">Add More Fields</button>
+    <div><input type="text" name="mytext[]"></div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
+
+        $(document).ready(function() {
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+    var add_button      = $(".add_field_button"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $("#rm").remove();
+
+            $(wrapper).append('<div id="divs"><input type="text" name="mytext[]"/>'); //add input box
+             $(wrapper).append('<div id="divs"><input type="text" name="mytext[]"/>'); //add input box
+              $(wrapper).append('<div id="divs"><input type="text" name="mytext[]"/></div>'); //add input box
+               $(wrapper).append('<div id="divs"><input type="text" name="mytext[]"/><a href="#" id="rm" class="remove_field">Remove</a></div>'); //add input box
+                $(wrapper).append('<br>')
+        }
+    });
+
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $("#divs").remove(); x--;
+        $("#divs").remove(); x--;
+        $("#divs").remove(); x--;
+        $("#divs").remove(); x--;
+
+    })
+});
--- resources/views/addrecipe.blade.php
+++ resources/views/addrecipe.blade.php
@@ -1,10 +1,42 @@
 @extends('layouts.app')
 
+
 @section('content')
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="row">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                    <form enctype="multipart/form-data" class="form-horizontal" method="POST" action="/profiel">
+                        {{ csrf_field() }}
+                        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                            <input type="file" name="recipe_picture">
+                            <input id="RecipeNaam" type="text" class="form-control" placeholder="Recipe name"><br>
+                            <input id="RecipeDescription" type="text" class="form-control"
+                                   placeholder="Recipe Description"><br>
+                        </div>
+
+
+                        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                            <input id="Ingredient" type="text" class="form-control" placeholder="Ingredient">
+                        </div>
+
+                        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                            <input id="voornaam" type="text" class="form-control" placeholder="Amount"><br>
+                        </div>
+
+                        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                            <select id="unit" name="unit">
+                                <option name="unit" value="ml">ml</option>
+                                <option name="unit" value="gram">gram</option>
+                                <option name="unit" value="tl">tl</option>
+                                <option name="unit" value="ml">el</option>
+                        </div>
 
 
 
-
-
-
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
